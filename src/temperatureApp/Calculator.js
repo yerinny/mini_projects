c0cb7023107c5678a1f20e5afe4ad21c70024035
yerinny <yerinlee90@gmail.com>
2020-03-24 14:53:49 -0700

    Add files via upload

---
+++ src/temperatureApp/Calculator.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import BoilingVerdict from './BoilingVerdict';
+import TemperatureInput from './TemperatureInput';
+
+class Calculator extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+          temperature: 20,
+          scale: 'C'
+        };
+    }
+  
+    handleChange(e,scale) {
+      this.setState({
+          temperature: e.target.value,
+            scale: scale
+        });
+    }
+  
+    render() {
+      const temperature = this.state.temperature;
+      const scale = this.state.scale;
+      let fTemp;
+      let cTemp;
+      if(scale === 'C'){
+          fTemp = Math.round((temperature * 9/ 5) +32);
+          cTemp = temperature;
+      }else if(scale === 'F'){
+          cTemp= Math.round((temperature - 32) * 5 / 9);
+          fTemp = temperature;
+      }
+
+
+      return (
+        <fieldset>
+          
+          <TemperatureInput temperature={fTemp} scale="F" handleChange={this.handleChange}/>
+          <TemperatureInput temperature={cTemp} scale="C" handleChange={this.handleChange}/>
+
+
+          <BoilingVerdict
+            celsius={parseFloat(cTemp)} />
+        </fieldset>
+      );
+    }
+  }
+
+export default Calculator;
